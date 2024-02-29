3ad711b937a264dde103e22c1661332dfb754405
Ubaid9513 <shaikhubaid2345@gmail.com>
2024-02-29 13:07:58 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,126 @@
+// alert('hello world');
+// console.log(alert + " wide")
+// alert (alert + " wide")
+// alert('Hello Javascript');
+
+// THIS IS STRING 
+var me =('hello javascript')
+alert (me + " guys")
+
+// THIS IS NUMBER
+var me =(9513)
+alert (me + " code")
+
+// THIS IS BOOLEAN
+let my = (true)
+alert (my + " done")
+
+// THIS IS null
+let ubaid = ("null")
+alert (ubaid)
+
+// THIS IS undefined
+let ub
+alert (ub)
+
+var u
+u = ["html", "css", "js"];
+
+u = {
+    name : "Ub", country : "Pak"
+}
+document.write(u);
+document.write("<br>");
+document.write(typeof u);
+
+
+/* Primitive Data Types
+ Number
+ String
+ Boolean
+ Null
+ Undefined
+*/
+
+
+// var my foo //illegal cannot use all special characters except $ and _
+// var my.foo //illegal
+// var 2myfoo //illegal cannot start with number
+// var var = 'hello' // illegal cannot use reserved keyword
+var $foo // legal  
+var _foo  //legal
+// var var = myFoo
+
+
+// var guyWhoKeepsSayingHisOwnName = 'Salman'; // camel case best practice
+// var guy_who_keeps_saying_his_own_name = 'Ahmed'; // snake case best practice
+
+// console.log(guyWhoKeepsSayingHisOwnName);
+var sum = 4 + 3.87348;
+
+
+
+// Basic arithmetic operations
+
+// var value = 5;
+
+// var sub = value - 2;
+
+// var mult = value * 3;
+
+// var division = value + 4/ 2; 
+
+// var ans = (value + 4)/ 2; 
+
+// var exponentiation = value ** 2 
+
+
+
+// alert(sum + " Sum of two numbers"); 
+// sum = sum + ' converted to string' // string concatination number + string type will be string
+
+// console.log("hello");
+
+// console.log(typeof(sum));
+
+
+
+
+// var num = 6;
+// console.log(num); // 6
+// num++; // 6  post increment
+// console.log(num); //7
+// console.log(num++); // 7
+// console.log(num); // 8
+// ++num; // 9 pre increment
+// console.log(num); // 9
+// console.log(++num);  // 10
+
+// practice decrement on your own
+// post decrement num--
+// pre decrement --num
+
+
+// var number = 6;
+// number = number + 5;
+// number += 5; // same as number = number + 5
+// number -= 5; // same as number = number - 5
+// number *= 5;
+// number /= 5;
+
+// 5  + '5' result will be 55  plus operator with string operands always concatination
+
+// 5 * '5' //results 25
+// 5 * '' //results 0;
+
+var val1 = +prompt('Enter Value'); // add + to convert String to  number if possible other wise results NaN
+var val2 = +prompt('Enter Value');
+console.log(val1 + val2);
+
+var logo = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ ";
+alert(logo)
+
+var val1 , val2 , val3
+var val=3
+var valu=5
+document.write(val + valu)
